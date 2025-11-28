c0e75f01f1a630478c97b8da4686c635cc2efd97
Thomas <devattom@gmail.com>
2025-11-28 14:26:37 +0100

    Ajout sequence 2

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,27 +2,24 @@ import {Component, inject} from '@angular/core';
 import {AsyncPipe} from '@angular/common';
 import {Task, Tasks} from '../../services/tasks';
 import {Observable} from 'rxjs';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   taskService = inject(Tasks);
+  tasks: Observable<Task[]> = this.taskService.tasks$;
+  newTaskLabel : string = '';
 
-  tasks: Observable<Task[]> = this.taskService.getTask();
-  interval: number | undefined;
-  count = 0;
+  ngOnInit() {}
 
-  ngOnInit() {
-    this.interval = setInterval(() => {
-      this.count ++;
-    }, 1000);
-  }
+  ngOnDestroy() {}
 
-  ngOnDestroy() {
-    clearInterval(this.interval);
+  addTask(label: string) {
+    this.taskService.addTask(label);
   }
 }
--- src/app/services/tasks.ts
+++ src/app/services/tasks.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {delay, of} from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 export type Task = {
   id: number;
@@ -9,13 +9,26 @@ export type Task = {
   providedIn: 'root',
 })
 export class Tasks {
-  tasks: Task[] = [
+  private taskSubject = new BehaviorSubject<Task[]>([
     {id: 1, label: 'Première tâche'},
     {id: 2, label: 'Deuxième tâche'},
     {id: 3, label: 'Troisieme tâche'},
-  ];
+  ]);
 
-  getTask() {
-    return of(this.tasks).pipe(delay(1000));
+  tasks$ = this.taskSubject.asObservable();
+
+  addTask(label: string) {
+    const currentTasks = this.taskSubject.getValue();
+
+    const lastId = currentTasks.length > 0
+      ? currentTasks[currentTasks.length - 1].id
+      : 0;
+
+    const newTask: Task = {
+      id: lastId + 1,
+      label: label,
+    };
+
+    this.taskSubject.next([...currentTasks, newTask]);
   }
 }
