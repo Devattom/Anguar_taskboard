910f8f0e7c5a73b08963ed023133fc314a98f30a
Thomas <devattom@gmail.com>
2026-01-21 13:28:18 +0100

    Auto-generated commit

---
+++ chunk-FFQB7HHE.js
@@ -0,0 +1,2 @@
+import{Ca as o,Wa as n,Xa as a,fb as s}from"./chunk-CTH24RMT.js";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-about"]],decls:2,vars:0,consts:[[1,"p-3"]],template:function(e,u){e&1&&(n(0,"div",0),s(1,` Page A Propos du TP pour Angular \xE9cole IPI, 2025.
+`),a())},encapsulation:2})};export{i as About};
